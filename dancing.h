7aaf829f32f385548925b0b3a3b08cd8bfec21e8
ejrh <ejrh00@gmail.com>
2021-04-01 16:45:14 +1300

    Handle solutions using a callback.

--- dancing.h
+++ dancing.h
@@ -28,8 +28,11 @@ typedef struct Matrix {
     Header root;
 
     /* Statistics. */
+    long int num_solutions;
     long int search_calls;
 } Matrix;
+
+typedef int (*Callback)(Matrix *matrix, Node **solution, int solution_size);
 
 
 #define foreachlink(h,a,x) for (x = (h)->a; x != (h); x = x->a)
@@ -40,7 +43,8 @@ extern Header *create_column(Matrix *matrix, int primary, char *fmt, ...);
 extern Node *create_node(Matrix *matrix, Node *after, Header *column);
 extern void destroy_matrix(Matrix *matrix);
 extern void print_matrix(Matrix *matrix);
-extern int search_matrix(Matrix *matrix, int depth, Node **solution);
+extern void print_solution(Node **solution, int solution_size);
+extern int search_matrix(Matrix *matrix, Callback solution_callback);
 
 
 #endif
